68d16b97ae075da247d646a10783495eade39237
Victor Hugo Barili Schons <hugoht3@hotmail.com>
2024-08-10 19:28:34 +0000

    taskmanager

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(elems, options);
+  });
+
+//   // Or with jQuery
+
+//   $(document).ready(function(){
+//     $('.sidenav').sidenav();
+//   });
